a07de6d8a564c7ef7714f9610b613a5410f47d89
PazzeG <guillaume.pazze@gmail.com>
2020-11-19 21:55:28 +0100

    master ready

--- elite-code/src/App.jsx
+++ elite-code/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import { Route } from 'react-router-dom';
 import '../src/fonts/Metropolis1920-Regular.ttf'
+import { Component } from 'react';
+import Nav from './Components/Header/Nav';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component  {
+  render(){
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;

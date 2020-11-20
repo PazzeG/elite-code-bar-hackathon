a9064f086bf50852f67e72441e2de0fa4d331caa
guillaume45 <couturier.g@gmail.com>
2020-11-20 11:31:11 +0100

    add files newId and connexionApp

--- elite-code/src/App.jsx
+++ elite-code/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import '../src/fonts/Metropolis1920-Regular.ttf'
 import { Component } from 'react';
 import Nav from './Components/Header/Nav';
+import NewId from './Components/New-id/NewId';
 
 class App extends Component  {
   render(){
---
+++ elite-code/src/Components/New-id/NewId.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './NewId.css';
+import { useForm } from 'react-hook-form';
+
+
+
+      
+
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="title">
+        <h1>Access request</h1>
+      </div>
+      <input type="text" placeholder="John" name="John" ref={register({required: true, min: 2, maxLength: 80})} />
+      <input type="text" placeholder="Doe" name="Doe" ref={register({required: true, maxLength: 100})} />
+      <input type="text" placeholder="Email" name="Email" ref={register({required: true, pattern: /^\S+@\S+$/i})} />
+      <input type="text" placeholder="address" name="address" ref={register} />
+      <input type="text" placeholder="City" name="City" ref={register} />
+
+      <input type="submit" />
+    </form>
+  );
+}
